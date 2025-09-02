e4f1cb26a9b45cd952a9bc1677da6d39077ecf58
ttoaffers <animeone56@gmail.com>
2025-09-03 02:03:31 +0800

    added a function for checking name formatting

--- src/Utils.java
+++ src/Utils.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Utils {
 
 
@@ -25,6 +27,35 @@ public class Utils {
                 continue;
             }
              return false;
+        }
+        return true;
+    }
+    public static boolean CheckNameFormat(String name){
+        String[] splits = name.trim().split(", ");
+        if(splits.length != 2){
+            return false;
+        }
+        String[] firstNameSplits = splits[1].split(" ");
+        ArrayList<Character> firstLetters = new ArrayList<>();
+        for(int i = 1; i < splits[0].length(); i++){
+            if(Character.isUpperCase(splits[0].charAt(i)))
+                return false;
+        }
+        firstLetters.add(splits[0].charAt(0));
+        for(String split: firstNameSplits){
+            firstLetters.add(split.charAt(0));
+        }
+        for (String firstName : firstNameSplits) {
+            for (int a = 1; a < firstName.length(); a++) {
+                if (Character.isUpperCase(firstName.charAt(a))) {
+                    return false;
+                }
+            }
+        }
+        for(char letter: firstLetters){
+            if(Character.isLowerCase(letter)){
+                return false;
+            }
         }
         return true;
     }
